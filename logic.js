f56e954e925526ae947cac16f203bdb3fa519c3c
randy brown <randy@beingbrown.net>
2014-04-13 18:27:34 -0700

    Persistence via IndexedDB added and drag and drop removed
    Drag and Drop and clock interaction are temporarily removed to focus on storing data acrosses uses. Persistence is a core functionality, and the interface was not intuitive based on testing.

--- logic.js
+++ logic.js
@@ -1,73 +1,91 @@
 window.onload = function()
 {
-  var pomodoro = new Timer(0.25);
-  var taskList = new Array();
+  var todoList = document.getElementById('tasklist');
+  var ul = document.createElement("ul");
+  
+//implement indexedDB file storage
+  var tasks = [
+    {label: "create to do list app"},
+    {label: "add things to list"},
+    {label: "?????????"},
+    {label: "profit"}
+    ];
+  
+  var db_name = "storedTasks";
+  var db_version = 1;
+  var db_store_name = "allTasks";
+  
+  var db;
 
-//generate a dummy array of tasks
-  for(var i = 0; i < 5; i++)
+  function openDB()
   {
-    taskList[i] = new Task();
-    taskList[i].setLabel("Item "+(i+1));
-  }
-
-//if there are any tasks, create a list and display them
-  if(taskList.length > 0)
-  {
-    var todoList = document.getElementById('tasklist');
-    var ul = document.createElement("ul");
-    for(var i = 0; i < taskList.length; i++)
+    console.log("Openning DB " + db_name);
+    var request = indexedDB.open(db_name, db_version);
+    request.onsuccess = function(event)
     {
-      var li = document.createElement("li");
-      li.id = taskList[i].getLabel();
-      li.draggable = true;
-      li.appendChild(document.createTextNode(taskList[i].getLabel()));
-      ul.appendChild(li);
+      db = this.result;
+      //getAllToDoItems
+    };
+    request.onerror = function(event)
+    {
+      console.log("DB Error: " + event.value);
+    };
+    request.onupgradeneeded = function(event)
+    {
+      console.log("upgrade needed on DB " + db_name);
+      var store = event.currentTarget.result.createObjectStore(db_store_name, {keyPath: 'id', autoincrement: true});
     }
-    todoList.appendChild(ul);
   }
-
-  document.getElementById('activeItem').ondrop = function(){dragDrop(event,pomodoro);};
-  document.getElementById('activeItem').ondragenter = cancel;
-  document.getElementById('activeItem').ondragover = cancel; //function(){return dragOver(event);};  
-
-  makeDraggable();
-}
-
-//Everything below here needs to be cleaned up
-function cancel(e) {
-  if (e.preventDefault) {
-    e.preventDefault();
-  }
-  return false;
-}
-
-function dragStart(ev)
-{
-  ev.dataTransfer.effectAllowed = 'move';
-  ev.dataTransfer.setData('Text',ev.target.id);
-  return true;
-}
-
-function dragDrop(ev,aTimer)
-{
-  var data = ev.dataTransfer.getData('Text');
-  ev.target.innerHTML = "";
-  ev.target.innerHTML = "<span draggable=\"true\">"+data+"</span>";
-  document.getElementById(data).style.display = "none";
-  aTimer.setFace(document.getElementById('timer'));
-  aTimer.stop();
-  aTimer.setDuration(0.25);
-  aTimer.face.onclick = function(){aTimer.togglePause();};
-  aTimer.countDown();
-  ev.stopPropagation();
-  return false;
-}
-
-function makeDraggable()
-{
-  var element = document.getElementsByTagName('li');
-  for(var i = 0; i < element.length; i++)
+  
+  function addToDo(label)
   {
-    element[i].ondragstart = function(){dragStart(event);};
+    var transaction = db.transaction(db_store_name,"readwrite");
+    var store = transaction.objectStore(db_store_name);
+    var request = store.put({"label" : label,"timeStamp" : new Date().getTime()});
+    request.onsuccess = function(event)
+    {
+      //getAllToDoItems
+    };
+    request.onerror = function(event)
+    {
+      console.log("DB Error: " + event.value);
+    };
   }
+    
+  function getAllToDos()
+  {
+    var trans = db.transaction(db_store_name,"readwrite");
+    var store = transaction.objectStore(db_store_name);
+      
+    var keyRange = IDBKeyRange.lowerBound(0);
+    var cursorRequest = store.openCursor(keyRange);
+      
+    cursorRequest.onsuccess = function(event)
+    {
+      var result = event.target.result;
+      if(!!result == false)
+        return;
+        
+      renderToDo(result.value);
+      result.continue();
+    };
+    cursorRequest.onerror = function(event)
+    {
+      console.log("Retrieving List Error: " + event.value);
+    };
+  }
+
+  function renderToDo(row)
+  {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(row.label));
+    ul.appendChild(li);
+  }
+
+  openDB();
+  for(var i in tasks)
+  {
+    renderToDo(tasks[i]);
+  }
+  todoList.appendChild(ul);
 }
