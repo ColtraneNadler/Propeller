09be14ca42ce1481e88d441982029acb9fd26487
randy brown <randy@beingbrown.net>
2014-04-18 21:58:19 -0700

    lots of changes to DataStore.js

--- DataStore.js
+++ DataStore.js
@@ -1,63 +1,178 @@
-function DataStore(newName)
+function DataStore(name)
 {
-  this.db = null;
-  this.name = newName;
-  this.version = 1;
-  this.stores = Array();
+  this.db = {};
+  this.name = name;
+  this.version = null;
+  this.objectStores = Array();
+  console.log("DataStore " + name + " version " + this.version + " created");
+  this.defaultValues = Array();
 }
 
-DataStore.prototype.addStore = function(newStoreName)
+DataStore.prototype.setDefaults = function(defaultValues)
 {
-  this.stores.push(newStoreName);
-  this.version++;
+    this.defaultValues = defaultValues;
 }
 
-DataStore.prototype.open = function(newStoreName)
+DataStore.prototype.open = function(callback,parameters)
 {
-  if(this.stores.indexOf(newStoreName) == -1)
+  console.log("Opening DataStore " + this.name);
+  if(!this.version)
   {
-    this.addStore(newStoreName);
+    this.version = 1;
   }
   var dataStore = this;
   var request = indexedDB.open(this.name, this.version);
-  request.onupgradeneeded = function(event)
-  {
-    console.log("UPGRADE NEEDED ON DATASTORE " + dataStore.name);
-    if(!event.currentTarget.result.objectStoreNames.contains(newStoreName))
-    {
-      var store = event.currentTarget.result.createObjectStore(newStoreName,{ keyPath: 'id', autoIncrement: true });
-    }
-  }
   request.onsuccess = function(event)
   {
-    console.log("SUCCESS!");
-    console.log(this);
+    console.log("DataStore " + dataStore.name + " version " + dataStore.version + " successfully opened.");
     dataStore.db = event.target.result;
-    console.log(dataStore.db);
-    dataStore.getData(newStoreName);
+    if(callback)
+    {
+      parameters.db = dataStore.db;
+      callback(parameters);
+    }
   }
   request.onerror = function(event)
   {
-    console.log("dataStore "+dataStore.name+" error: " + event.value);
+    console.log("ERROR OPENING DATASTORE " + dataStore.name + " version " + dataStore.version + ": " + event.value);
+  }
+  request.onupgradeneeded = function(event)
+  {
+    console.log("UPGRADE NEEDED ON DATASTORE " + dataStore.name);
+    for(var i = 0; i < event.target.result.objectStoreNames.length; i++)
+    {
+      if(dataStore.objectStores.indexOf(event.target.result.objectStoreNames.item(i)) == -1)
+      {
+        console.log("DELETING OBJECT STORE " + event.target.result.objectStoreNames.item(i));
+        event.target.result.deleteObjectStore(event.target.result.objectStoreNames.item(i));
+      }
+    }
+    for(i in dataStore.objectStores)
+    {
+      if(!event.currentTarget.result.objectStoreNames.contains(dataStore.objectStores[i]))
+      {
+        console.log("Creating object store " + dataStore.objectStores[i]);
+        var store = event.currentTarget.result.createObjectStore(dataStore.objectStores[i],{ "autoIncrement" : true });
+      }
+    }
   }
 }
 
-DataStore.prototype.getData = function(newStoreName)
+DataStore.prototype.createObjectStore = function(objectStore)
 {
-  console.log(this.db);
-  var transaction = this.db.transaction(newStoreName,"readwrite");
-  var store = transaction.objectStore(newStoreName);
-  var keyRange = IDBKeyRange.lowerBound(0);
-  var cursorRequest = store.openCursor(keyRange);
-  cursorRequest.onsuccess = function(event)
+  if(Array.isArray(objectStore))
   {
-    var result = event.target.result;
-    if(!!result == false)
-        return;
-      result.continue();
+    for(i in objectStore)
+    {
+       this.objectStores.push(objectStore[i]);
+    }
   }
-  cursorRequest.onerror = function(event)
+  else
   {
-    console.log("LIST RETRIEVAL ERROR: " + event.value.label);
+    this.objectStores.push(objectStore);
   }
+  this.bumpVersion();
+}
+
+DataStore.prototype.deleteObjectStore = function(objectStore)
+{
+  if(Array.isArray(objectStore))
+  {
+    for(i in objectStore)
+    {
+      if(this.objectStores.indexOf(objectStore[i]) > -1)
+      {
+        this.objectStores.splice(objectStore[i], 1);
+      }
+    }
+  }
+  else
+  {
+    if(this.objectStores.indexOf(objectStore) > -1)
+    {
+      this.objectStores.splice(objectStore, 1);
+    }
+  }
+  this.bumpVersion();
+}
+
+DataStore.prototype.createObject = function(object,objectStore)
+{
+  var dataStore = this;
+  if(Array.isArray(object))
+  {
+    for(i in object)
+    {
+      object[i].timeStamp = new Date().getTime();
+    }
+  }
+  else
+  {
+    object.timeStamp = new Date().getTime();
+  }
+  var parameters = {"objectStore" : objectStore,"object" : object};
+  this.open(dataStore.addObject,parameters);
+}
+
+DataStore.prototype.addObject = function(parameters)
+{
+  if(parameters.hasOwnProperty("objectStore"))
+  {
+    console.log("Adding Objects to " + parameters.objectStore + " object Store.");
+    var transaction = parameters.db.transaction(parameters.objectStore,"readwrite");
+    var store = transaction.objectStore(parameters.objectStore);
+    if(Array.isArray(parameters.object))
+    {
+      for(i in parameters.object)
+      {
+        var request = store.put(JSON.stringify(parameters.object[i]));    
+      }
+    }
+    else
+    {
+      var request = store.put(JSON.stringify(parameters.object));
+    }
+  }
+}
+
+DataStore.prototype.dumpObjectStore = function(objectStore)
+{
+  var dataStore = this;
+  var parameters = { "objectStore" : objectStore };
+  this.open(dataStore.retrieveObject,parameters);
+}
+
+DataStore.prototype.retrieveObject = function(parameters)
+{
+  if(parameters.hasOwnProperty("objectStore"))
+  {
+    var dataStore = parameters.db;
+    var transaction = parameters.db.transaction(parameters.objectStore,"readwrite");
+    var store = transaction.objectStore(parameters.objectStore);
+    var keyRange = IDBKeyRange.lowerBound(0);
+    var cursorRequest = store.openCursor(keyRange);
+    cursorRequest.onsuccess = function(event)
+    {
+      return event.target.result;
+    }
+    cursorRequest.onerror = function(event)
+    {
+      console.log("LIST RETRIEVAL ERROR: " + event.value.label);
+    }
+  }
+}
+
+DataStore.prototype.delete = function()
+{
+  console.log("DELETING DATASTORE " + this.name);
+  indexedDB.deleteDatabase(this.name);
+  console.log("DATASTORE " + this.name + " DELETED.");
+}
+
+DataStore.prototype.bumpVersion = function(version)
+{
+  if(this.version && version)
+    this.version = version;
+  else if(this.version)
+    this.version++;
 }
