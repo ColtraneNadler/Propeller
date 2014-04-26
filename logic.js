3bc66fdb072e09bde3a0967ec5ee6c3c654d78e9
randy brown <randy@beingbrown.net>
2014-04-25 23:31:00 -0700

    logic to match localstorage

--- logic.js
+++ logic.js
@@ -1,11 +1,29 @@
 window.onload = function()
 {
-//things go here
-  var taskList = new DataStore("propeller_DataStore");
-  var masterTaskList = new ObjectStore("masterTaskList");
-  masterTaskList.setDefaultEntry([{"label" : "create to do list app"},{"label" : "add things to list"},{"label" : "?????????"},{"label" : "profit"}]);
-  taskList.addObjectStore(masterTaskList);
-  taskList.open();
-  
-  console.log(taskList.results);
+   var ls = new LocalStore();  
+  ls.getData("taskList",printList);
+}
+
+function printList(items)
+{
+  var ls = new LocalStore();
+  ls.data['taskList'] = items;
+  var todoList = document.getElementById('tasklist');
+  var ul = document.createElement("ul");
+  for(i in items)
+  {
+    var li = document.createElement("li");
+//    li.id = taskList[i].getLabel();
+//    li.draggable = true;
+    var taskText = document.createTextNode(items[i].label)
+    var a = document.createElement("a");
+    a.id = i;
+    a.onclick = function(event){document.getElementById(event.target.id).parentNode.style.display = "none";ls.deleteItem("taskList",event.target.id);console.log(event.target.id);};
+    var remove = document.createTextNode("[ X ]");
+    a.appendChild(remove);
+    li.appendChild(taskText);
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
+  todoList.appendChild(ul);
 }
