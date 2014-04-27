7a1144b0ed5164cefec77e01dca3f7e0a553fe51
randy brown <randy@beingbrown.net>
2014-04-26 22:42:17 -0700

    localstore implementation complete
    . not feature complete, but localstore aspects are complete
    
    . things left to be implemented further up the tree:
    .  session and user stat tracking
    .  task object integration
    .  timer integration

--- LS-DataStore.js
+++ LS-DataStore.js
@@ -1,61 +1,48 @@
 function LocalStore(callback)
 {
   this.data = {};
-//  this.getUser();
 }
 
-LocalStore.prototype.getUser = function()
+LocalStore.prototype.getUser = function(callback)
 {
   var ls = this;
-  chrome.storage.local.get(null,function(result)
-                                  {
-//                                    console.log(Object.keys(result).indexOf("USER"));
-//                                    console.log(result.hasOwnProperty("USER"));
-//                                    console.log(Object.keys(result).length);
-                                    if(Object.keys(result).indexOf("USER") < 0)
-                                    {
-                                      console.log("no result");
-                                      ls.data["USER"] = {"name": "user1"};
-                                      ls.data["TASKLIST"] = [{"label" : "create to do list app"},{"label" : "add things to list"},{"label" : "?????????"},{"label" : "profit"},{"label" : "share propeller with your friends"}];
-                                    }
-                                    else
-                                    {
-                                      console.log("result found: " + result.USER.name);
-                                      ls.data.USER = result.USER;
-                                      ls.data.TASKLIST = result.TASKLIST;
-                                    }
-//                                    console.log("Local User: " + ls.data.USER.name);
-//                                    console.log("TASK LIST: " + ls.data.TASKLIST);
-                                    chrome.storage.local.set(ls.data);
-                                  }
-                          );
+  chrome.storage.local.get('USER',function(result){ls.data.USER = result.USER;callback(!!Object.keys(result).length);});
+}
+
+LocalStore.prototype.setDefaults = function(callback)
+{
+  var ls = this;
+  ls.data.USER = {"name" : "user1"};
+  ls.data.TASKLIST = [{"label" : "create to do list app"},{"label" : "add things to list"},{"label" : "?????????"},{"label" : "profit"},{"label" : "share propeller with your friends"}];
+  chrome.storage.local.set(ls.data,returnDefaults);
+  
+  function returnDefaults()
+  {
+    callback(ls.data.TASKLIST,ls);
+  }
 }
 
 LocalStore.prototype.getData = function(key,callback)
 {
   var ls = this;
-  if(Object.keys(ls.data).indexOf(key) < 0)
+  chrome.storage.local.get(key,function(result){ls.data[key] = result[key];callback(result[key],ls);});
+}
+
+LocalStore.prototype.deleteItem = function(key,value,callback)
+{
+  this.data[key].splice(value,1);
+  //this needs a callback
+  chrome.storage.local.set(this.data);
+}
+
+LocalStore.prototype.addItem = function(key,value,callback)
+{
+  var ls = this;
+  this.data[key].push(value);
+  chrome.storage.local.set(this.data,returnData);
+  
+  function returnData()
   {
-//    console.log("not found");
-    chrome.storage.local.get(null,function(result)
-                                  {
-                                    if(Object.keys(result).indexOf(key) < 0)
-                                    {
-                                      console.log("no result");
-                                    }
-                                    else
-                                    {
-                                      console.log(result[key]);
-                                      ls.data[key] = result[key];
-                                      callback(ls.data[key]);
-//                                      console.log(ls.data.hasOwnProperty(key));
-//                                      console.log(ls.data[key]);
-                                    }
-                                  }
-                             );
-  }
-  else
-  {
-    console.log(Object.keys(this.data).indexOf(key));
+    callback(ls.data[key],ls);
   }
 }
--- logic.js
+++ logic.js
@@ -1,13 +1,47 @@
 window.onload = function()
 {
-  var ls = new LocalStore();  
-  ls.getData("TASKLIST",printList);
+  var ls = new LocalStore();
+  ls.getUser(verifyUser);
+  var todoIn = document.getElementById('task-in');
+  todoIn.onkeydown = addTask;
+  
+  function verifyUser(exists)
+  {
+    if(!exists)
+    {
+      return ls.setDefaults(printList); 
+    }
+    else
+    {
+      ls.getData('TASKLIST',printList);
+    }
+  }
+  
+  function addTask(event)
+  {
+    if(event.keyCode == 13)
+    {
+      var task = {"label" : event.target.value};
+      ls.addItem("TASKLIST",task,printList);
+      event.target.value = null;
+    }
+  }
 }
 
-function printList(items)
+function loadItems(items)
+{
+  console.log(items);
+}
+
+function printList(items,ls)
 {
   var todoList = document.getElementById('tasklist');
+  while(todoList.firstChild)
+  {
+    todoList.removeChild(todoList.firstChild);
+  }
   var ul = document.createElement("ul");
+  console.log(items);
   for(i in items)
   {
     var li = document.createElement("li");
@@ -16,6 +50,7 @@ function printList(items)
     var taskText = document.createTextNode(items[i].label)
     var a = document.createElement("a");
     a.id = i;
+    a.onclick = function(event){document.getElementById(event.target.id).parentNode.style.display = "none";ls.deleteItem("TASKLIST",event.target.id);console.log(event.target.id);};
     var remove = document.createTextNode("[ X ]");
     a.appendChild(remove);
     li.appendChild(taskText);
@@ -23,4 +58,9 @@ function printList(items)
     ul.appendChild(li);
   }
   todoList.appendChild(ul);
+}
+
+function printWorld(items)
+{
+  console.log(items);
 }
