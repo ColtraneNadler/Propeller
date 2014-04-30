f6690795921efa5cd597bee268402e79e7bbaddc
randy brown <randy@beingbrown.net>
2014-04-29 21:50:43 -0700

    Added depth to Task Class
    . Task class now has an ID to move away from numerical ids.
    . active trait is added to move from hard to soft deletes of tasks for data tracking
    . All attributes of the class are declared and set to an initial value in teh constructor.
    
    . This will allow the implementation of a few key task features over time:
    .  tracking of number of tasks created and deleted.
    .  Active attribute has extensibility for repeated tasks.
    .  between clock and time traits, active and latent time can be tracked
    
    An actual user class will need to be created to track many derivative stats

--- Task.js
+++ Task.js
@@ -1,24 +1,47 @@
 function Task(label)
 {
   this.timer = new Timer(0.25);
-  this.complete = false;
-  this.label = label;
-  this.creationTime = new Date().getTime();
-}
 
+  this.id =  Math.random().toString(36).substr(2, 5);
+  this.label = label;
+
+  this.active = true;
+  this.complete = false;
+  
+  this.creationTime = new Date().getTime();
+  this.completionTime = null;
+}
+Task.prototype.setTimer = function(timer)
+{
+  this.timer = timer;
+}
+Task.prototype.getTimer = function()
+{
+  return this.timer;
+}
+Task.prototype.setId = function(id)
+{
+  this.id = id;
+}
+Task.prototype.getId = function()
+{
+  return this.id;
+}
 Task.prototype.setLabel = function(aLabel)
 {
   this.label = aLabel;
 }
-
 Task.prototype.getLabel = function()
 {
   return this.label;
 }
-
-Task.prototype.setTime = function(aTime)
+Task.prototype.setActive = function(active)
 {
-  this.timer.setDuration(aTime);
+  this.active = active;
+}
+Task.prototype.getActive = function()
+{
+  return this.active;
 }
 Task.prototype.setComplete = function(complete)
 {
@@ -27,4 +50,24 @@ Task.prototype.setComplete = function(complete)
   {
     this.completionTime = new Date().getTime();
   }
+}
+Task.prototype.getComplete = function()
+{
+  return this.complete;
+}
+Task.prototype.setCreationTime = function(creationTime)
+{
+  this.creationTime = creationTime;
+}
+Task.prototype.getCreationTime = function()
+{
+  return this.creationTime;
+}
+Task.prototype.setCompletionTime = function(completionTime)
+{
+  this.completionTime = completionTime;
+}
+Task.prototype.getCompletionTime = function()
+{
+  return this.completionTime;
 }
