31f2aa13fa503bf32d029a0d4bd83ebde871ac73
randy brown <randy@beingbrown.net>
2014-07-15 09:50:36 -0700

    added basic persistence. Needs to be decoupled

--- app.js
+++ app.js
@@ -1,4 +1,72 @@
-window.onload = function() {
+function Event(element,trigger,action) {
+  this.element = element || ""
+  this.trigger = trigger || ""
+  this.action = action || ""
+}
+
+function View() {
+  this.head = ""
+  this.menu = ""
+  this.body = ""
+  this.foot = ""
+
+  this.events = []
+}
+
+View.prototype.signal = function(message) {
+  return message
+}
+
+View.prototype.receive = function(message) {
+  if(Array.isArray(message)) {
+    for(var i = 0; i < message.length; i++) {
+      createListItem(message[i])
+    }
+  } else {
+    createListItem(message)
+  }
+
+  function createListItem(message) {
+    var li = document.createElement("li")
+    var cb = document.createElement("input")
+    cb.setAttribute("type","checkbox")
+    li.appendChild(cb)
+    li.appendChild(document.createTextNode(message))
+    document.getElementById("to_do_list").appendChild(li)
+  }
+}
+
+function App() {
+  this.store = window.localStorage || ""
+  this.data =  this.store["tasks"] || []
+  this.data = JSON.parse(this.data)
+
+  this.head = ""
+  this.menu = ""
+  this.body = ""
+  this.foot = ""
+
+  this.views = []
+}
+
+App.prototype.show = function(view) {
+  this.head.innerHTML = view.head
+  this.menu.innerHTML = view.menu
+  this.body.innerHTML = view.body
+  this.foot.innerHTML = view.foot
+
+  view.receive(this.data);
+
+  for(var i = 0; i < view.events.length; i++) {
+    document.getElementById(view.events[i].element).addEventListener(view.events[i].trigger,view.events[i].action)
+  }
+}
+
+App.prototype.receive = function(message) {
+  this.data.push(message)
+  this.store["tasks"] = JSON.stringify(this.data)
+}
+
 //register a new app to the window
   propeller = new App()
 
@@ -16,10 +84,9 @@ window.onload = function() {
 
 //register any events
   basicView.events.push(new Event("input","keydown",function(event) {
-                                            if(event.keyCode == 13) {
-                                              var li = document.createElement("li")
-                                              li.appendChild(document.createTextNode(event.target.value))
-                                              document.getElementById("to_do_list").appendChild(li)
+                                            if(event.keyCode == 13 && event.target.value != "") {
+                                              basicView.receive(event.target.value)
+                                              propeller.receive(basicView.signal(event.target.value))
                                               event.target.value = ""
                                             }
                                           }))
@@ -35,39 +102,3 @@ window.onload = function() {
 //register views and set a view to display
   propeller.views.push(basicView)
   propeller.show(basicView)
-}
-
-function App() {
-  this.head = ""
-  this.menu = ""
-  this.body = ""
-  this.foot = ""
-
-  this.views = []
-}
-
-App.prototype.show = function(view) {
-  this.head.innerHTML = view.head
-  this.menu.innerHTML = view.menu
-  this.body.innerHTML = view.body
-  this.foot.innerHTML = view.foot
-
-  for(var i = 0; i < view.events.length; i++) {
-    document.getElementById(view.events[i].element).addEventListener(view.events[i].trigger,view.events[i].action)
-  }
-}
-
-function View() {
-  this.head = ""
-  this.menu = ""
-  this.body = ""
-  this.foot = ""
-
-  this.events = []
-}
-
-function Event(element,trigger,action) {
-  this.element = element || ""
-  this.trigger = trigger || ""
-  this.action = action || ""
-}
