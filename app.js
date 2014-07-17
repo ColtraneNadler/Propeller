216c872f28a770b832483c51aa1da5acb0fdaca6
randy brown <randy@beingbrown.net>
2014-07-16 17:13:41 -0700

    still need to fix signalling and receiving

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+function Store(point) {
+  this.store = window.localStorage || ""
+  this.point = point || "appData"
+
+  this.data = (this.store[point] ? JSON.parse(this.store[point]) : [])
+}
+
 function Event(element,trigger,action) {
   this.element = element || ""
   this.trigger = trigger || ""
@@ -5,6 +12,8 @@ function Event(element,trigger,action) {
 }
 
 function View() {
+  this.label = ""
+
   this.head = ""
   this.menu = ""
   this.body = ""
@@ -13,33 +22,28 @@ function View() {
   this.events = []
 }
 
-View.prototype.signal = function(message) {
-  return message
+View.prototype.registerTransmitter = function(transmitter) {
+  this.transmit = transmitter
 }
 
-View.prototype.receive = function(message) {
-  if(Array.isArray(message)) {
-    for(var i = 0; i < message.length; i++) {
-      createListItem(message[i])
-    }
-  } else {
-    createListItem(message)
-  }
-
-  function createListItem(message) {
-    var li = document.createElement("li")
-    var cb = document.createElement("input")
-    cb.setAttribute("type","checkbox")
-    li.appendChild(cb)
-    li.appendChild(document.createTextNode(message))
-    document.getElementById("to_do_list").appendChild(li)
+View.prototype.send = function() {
+  if(this.transmit) {
+    return this.transmit()
   }
 }
 
-function App() {
-  this.store = window.localStorage || ""
-  this.data =  this.store["tasks"] || []
-  this.data = JSON.parse(this.data)
+View.prototype.registerReceiver = function(receiver) {
+  this.receive = receiver
+}
+
+View.prototype.get = function(message) {
+  if(this.receive) {
+    this.receive(message)
+  }
+}
+
+function App(dataPoint) {
+  this.store = new Store(dataPoint) || {}
 
   this.head = ""
   this.menu = ""
@@ -49,26 +53,47 @@ function App() {
   this.views = []
 }
 
+App.prototype.getViewFromLabel = function(label) {
+  for(var i = 0; i < this.views.length; i++ ) {
+    if(this.views[i].label == label) {
+      return this.views[i]
+    }
+  }
+  return null
+}
+
+//more to come here
+App.prototype.setActiveView = function(view) {
+  view.get(this.store.data)
+
+  this.show(view)
+}
+
 App.prototype.show = function(view) {
   this.head.innerHTML = view.head
   this.menu.innerHTML = view.menu
   this.body.innerHTML = view.body
   this.foot.innerHTML = view.foot
 
-  view.receive(this.data);
-
   for(var i = 0; i < view.events.length; i++) {
     document.getElementById(view.events[i].element).addEventListener(view.events[i].trigger,view.events[i].action)
   }
 }
 
+//this function needs some work
 App.prototype.receive = function(message) {
-  this.data.push(message)
-  this.store["tasks"] = JSON.stringify(this.data)
+  this.store.data.push(message)
+  this.store.store[this.store.point] = JSON.stringify(this.store.data)
+  this.show(this.getViewFromLabel("basicView"))
 }
 
+
+
+
+
+window.onload = function() {
 //register a new app to the window
-  propeller = new App()
+  propeller = new App("tasks")
 
 //registering app elements might require a function later
   propeller.head = document.getElementsByTagName("header")[0]
@@ -78,15 +103,39 @@ App.prototype.receive = function(message) {
 
 //create the views and set their content
   basicView = new View()
+  basicView.label = "basicView"
   basicView.head = "<h1>Propeller</h1>"
   basicView.body = "<input type=\"text\" id=\"input\" value=\"walk the dog\" />" +
                    "<ul id=\"to_do_list\"></ul>"
 
+  basicView.registerReceiver(function(message) {
+                               if(Array.isArray(message)) {
+                                 for(var i = 0; i < message.length; i++) {
+                                   addListItem(this,message[i])
+                                 }
+                               } else {
+                                 addListItem(this,message)
+                               }
+                               function addListItem(view,item) {
+                                 var li = document.createElement("li")
+                                 var cb = document.createElement("input")
+                                 cb.setAttribute("type","checkbox")
+                                 li.appendChild(cb)
+                                 li.appendChild(document.createTextNode(item))
+
+                                 var temp = document.createElement("div")
+                                 temp.innerHTML = view.body
+                                 temp.childNodes[1].appendChild(li)
+                                 view.body = temp.innerHTML
+                               }
+                             })
+
 //register any events
   basicView.events.push(new Event("input","keydown",function(event) {
                                             if(event.keyCode == 13 && event.target.value != "") {
-                                              basicView.receive(event.target.value)
-                                              propeller.receive(basicView.signal(event.target.value))
+                                              //this part needs to change
+                                              basicView.get(event.target.value)
+                                              propeller.receive(event.target.value)
                                               event.target.value = ""
                                             }
                                           }))
@@ -101,4 +150,5 @@ App.prototype.receive = function(message) {
 
 //register views and set a view to display
   propeller.views.push(basicView)
-  propeller.show(basicView)
+  propeller.setActiveView(basicView)
+}
