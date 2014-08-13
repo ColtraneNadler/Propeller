9e6e3dae284bcd27c47217f5155194d3c0534d62
randy brown <randy@beingbrown.net>
2014-08-12 20:15:24 -0700

    adjusting propeller.js to baseplate

--- propeller.js
+++ propeller.js
@@ -1,11 +1,22 @@
 window.onload = function() {
-  propeller = new App("Propeller")
+  propeller = new App()
+  propeller.registerStore(new Store("propeller"))
+  propeller.default = basicView
 
   propeller.head = document.getElementsByTagName("header")[0]
   propeller.menu = document.getElementsByTagName("nav")[0]
-  propeller.body = document.getElementsByTagName("section")[0]
+//I don't like the way that body is set now
+  propeller.body = document.body
+//  propeller.body = document.getElementsByTagName("section")[0]
   propeller.foot = document.getElementsByTagName("footer")[0]
 
+  propeller.signal = function() {
+  }
+
+  propeller.update = function() {
+  }
+/**
+//I'm not sure how all this will work from now on
   propeller.registerView(basicView,false)
   propeller.registerView(addTask,true)
   propeller.registerView(addTag,true)
@@ -13,5 +24,8 @@ window.onload = function() {
   propeller.registerView(tagList,true)
   propeller.registerView(taskStats,true)
 
+  prop.add(BasicView)
+
   propeller.setActiveView(basicView)
+**/
 }
