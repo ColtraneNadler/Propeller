8382c98cec7d8621c02a3cd9e49fead3abdee2fc
randy brown <randy@beingbrown.net>
2014-10-11 10:25:23 -0700

    Tags can be added to objects
    Known issues:
    id overlap needs to be adjusted
    checks don't stay on tags
    no way to view tag task lists

--- AddTask.js
+++ AddTask.js
@@ -2,8 +2,60 @@ addTask = new View("Add Task")
 addTask.type = "modal"
 
 addTask.set("head","<h1>Propeller</h1>")
-addTask.set("body","<input type=\"text\" id=\"at_input\" value=\"walk the dog\" autofocus=\"autofocus\"/>")
+addTask.set("body","<input type=\"text\" id=\"at_input\" value=\"walk the dog\" autofocus=\"autofocus\"/>" +
+            "<ul id=\"tag_list\"></ul>")
 addTask.set("foot","<input type=\"button\" id=\"at_close\" value=\"finished\"/>")
+
+addTask.registerReceiver(
+  function(state) {
+    var tag_list = this.body.querySelector("#tag_list")
+    while(tag_list.firstChild) {
+      tag_list.removeChild(tag_list.firstChild)
+    }
+    while(this.events.length > 4) {
+      this.events.pop()
+    }
+    if(state.tag && state.tag.length > 0) {
+      buildList(this,"#tag_list",state.tag)
+    }
+
+    function buildList(view, target, item) {
+      for(var i = 0; i < item.length; i++) {
+        if(item[i].active) {
+          var li = createListItem(item[i])
+          addListItem(view,target,li)
+        }
+      }
+    }
+
+    function createListItem(item) {
+      var li = document.createElement("li")
+      var tag = document.createElement("span")
+      var ctrl = document.createElement("span")
+      var check = document.createElement("input")
+
+      li.id = "tag_" + item.id
+      tag.className = "label"
+      check.id = "check_" + item.id
+
+      check.setAttribute("type","checkbox")
+
+
+      ctrl.appendChild(check)
+      tag.appendChild(document.createTextNode(item.label))
+
+      li.appendChild(tag)
+      li.appendChild(ctrl)
+
+      return li
+    }
+
+    function addListItem(view,target,li) {
+      view.body.querySelector(target).appendChild(li)
+    }
+
+  }
+)
 
 addTask.events.push(new Event("at_input","keydown",
   function(event) {
@@ -15,13 +67,13 @@ addTask.events.push(new Event("at_input","keydown",
 
 //tag.tasks might be better than tasks.tags
 //make tasks an attribute of tags?
-//      var tags = event.target.parentNode.querySelector("#tag_list")
-//      for(var i = 0; i < tags.children.length; i++) {
-//        var id = tags.children[i].id.substr("tag_".length)
-//        if(tags.children[i].querySelector("#co_" + id).checked) {
-//          task.tags[id] = true
-//        }
-//      }
+      var tags = event.target.parentNode.querySelector("#tag_list")
+      for(var i = 0; i < tags.children.length; i++) {
+        var id = tags.children[i].id.substr("tag_".length)
+        if(tags.children[i].querySelector("#check_" + id).checked) {
+          task.tags[id] = true
+        }
+      }
       event.target.value = ""
       return new Message("task","create",task)
     }
