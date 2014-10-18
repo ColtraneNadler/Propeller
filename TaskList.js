9a09da7667626c968e3d63a993f25dbb4aaa347f
randy brown <randy@beingbrown.net>
2014-10-17 22:18:13 -0700

    Tags are a menu on Tasks

--- TaskList.js
+++ TaskList.js
@@ -8,14 +8,59 @@ taskList.set("foot","")
 taskList.registerReceiver(
   function(state) {
     var task_list = this.body.querySelector("#task_list")
+    var tag_list = this.menu.querySelector("#tag_list")
+
     while(task_list.firstChild) {
       task_list.removeChild(task_list.firstChild)
     }
+    while(tag_list.firstChild) {
+      tag_list.removeChild(tag_list.firstChild)
+    }
+
     while(this.events.length > 0) {
       this.events.pop()
     }
+
+    if(state.tag && state.tag.length > 0) {
+      buildMenu(this,"#tag_list",state.tag)
+    }
     if(state.task && state.task.length > 0) {
       buildList(this,"#task_list",state.task)
+    }
+
+    function buildMenu(view,target,item) {
+      for(var i = 0; i < item.length; i++) {
+        if(item[i].active) {
+          var li = createMenuItem(item[i])
+//          console.log(li)
+          createMenuEvents(view,item[i])
+          addMenuItem(view,target,li)
+        }
+      }
+    }
+
+    function createMenuItem(item) {
+      var li = document.createElement("li")
+
+      li.id = "tag_" + item.id
+      li.appendChild(document.createTextNode(item.label))
+
+      return li
+    }
+
+    function createMenuEvents(view,item) {
+      var setActiveTag = new Event()
+      setActiveTag.element = "tag_" + item.id
+      setActiveTag.trigger = "click"
+      setActiveTag.action = function(event) {
+        return new Message("activeTag","update",item.label)
+      }
+
+      view.events.push(setActiveTag)
+    }
+
+    function addMenuItem(view,target,item) {
+      view.menu.querySelector(target).appendChild(item)
     }
 
     function buildList(view,target, item) {
