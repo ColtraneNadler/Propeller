a80f31881e1e7289e0d4455f120d6cf29bbce98e
randy brown <randy@beingbrown.net>
2014-11-04 21:55:11 -0800

    the lamest commit ever

--- ActiveTask.js
+++ ActiveTask.js
@@ -2,5 +2,5 @@ activeTask = new View("Active Task")
 
 activeTask.set("head","<h1>Propeller</h1>")
 activeTask.set("menu","")
-activeTask.set("body","")
+activeTask.set("body","<h2 id=\"activeTask\"></h2><div id=\"clockFace\"></div>")
 activeTask.set("foot","")
