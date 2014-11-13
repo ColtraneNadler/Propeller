a2c519039de83c8830c06520de731d76104c236d
randy brown <randy@beingbrown.net>
2014-11-12 20:41:51 -0800

    basic clockface posting

--- ActiveTask.js
+++ ActiveTask.js
@@ -9,16 +9,22 @@ activeTask.registerReceiver(
   function(state) {
     if(state.activeTask) {
       var task = this.body.querySelector("#activeTask")
+      var clock = this.body.querySelector("#clockFace")
       var match = false
 
       while(task.firstChild) {
         task.removeChild(task.firstChild)
       }
 
+      while(clock.firstChild) {
+        clock.removeChild(clock.firstChild)
+      }
+
       for(var i = 0; i < state.task.length && !match; i++) {
         match = state.task[i].id == state.activeTask
         if(match) {
           task.appendChild(document.createTextNode(state.task[i].label))
+          clock.appendChild(document.createTextNode(state.task[i].requiredTime - state.task[i].workSessions.reduce(function(a,b) {return a + b },0)))
         }
       }
     }
