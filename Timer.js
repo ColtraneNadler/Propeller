547a02b20bc44784a8bb488652eb7a168d977b29
randy brown <randy@beingbrown.net>
2014-12-27 09:49:14 -0800

    Timer is a data object
    In order to be a part of state, Timer cannot have functions
    Timer functions will be moved to the appropriate view

--- Timer.js
+++ Timer.js
@@ -1,32 +1,8 @@
 function Timer() {
-  this.duration = ""
-  this.timeLeft = duration * 60 * 1000
+  this.duration = 0
+  this.timeLeft = this.duration * 60 * 1000
   this.timerEnd = 0
 
   this.running = false
   this.timerID = 0
-
-  function tick() {
-    timeLeft = timerEnd - (new Date().getTime())
-    if(timeLeft > 0) {
-      //tell the time left
-    } else {
-      stop()
-    }
-  }
-
-  function stop() {
-    clearInterval(timerID)
-    timeLeft = 0
-    isRunning = false
-  }
-
-  return {
-    countDown: function() {
-      if(!running) {
-        running = true
-        timerEnd = new Date().getTime() + timeLeft
-      }
-    }
-  }
 }
